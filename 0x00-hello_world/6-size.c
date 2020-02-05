345ed7f06e4dfb76eb22558605bc8a1a3b16c6e9
cbarros7 <cj.barros7@gmail.com>
2020-02-05 13:32:24 -0500

    This commit is for 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,10 @@
-#!/bin/bash
-#include <stdio.h> 
-
-int main ()
+#include <stdio.h>
+int main(void)
 {
-
-    printf ("Size of a char: %i byte(s)\n", sizeof (char));
-    printf ("Size of an int: %i byte(s)\n", sizeof (int));
-    printf ("Size of a long int: %i byte(s)\n", sizeof (long int));
-    printf ("Size of a a long long int: %i byte(s)\n", sizeof (long long int));
-    printf ("Size of a a float: %i byte(s)\n", sizeof (float));
-
-    return 0;
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a a long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of a a float: %i byte(s)\n", sizeof(float));
+return (0);
 }
