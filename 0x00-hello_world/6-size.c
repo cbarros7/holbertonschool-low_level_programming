7d1be42b1208fc145d6d4804e021bf10323d962b
cbarros7 <cj.barros7@gmail.com>
2020-02-05 15:49:33 -0500

    This commit is for 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,6 @@
  */
 int main(void)
 {
-int intType;
-float floatType;
-char charType;
-long int longintType;
-long long int long_long_Type;
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long));
