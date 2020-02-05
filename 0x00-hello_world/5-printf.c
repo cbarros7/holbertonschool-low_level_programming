bd1531552a58bc0be361736b2b766073ea5cf4d2
cbarros7 <cj.barros7@gmail.com>
2020-02-05 13:32:09 -0500

    This commit is for 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,6 @@
-#!/bin/bash
-#include <stdio.h> 
-
-int main ()
+#include <stdio.h>
+int main(void)
 {
-    printf ("with proper grammar, but the outcome is a piece of art,\n");
-
-    return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
