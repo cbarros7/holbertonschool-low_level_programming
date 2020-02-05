87ef6f43e3f8df325afdcb336cf64428cbe8db94
cbarros7 <cj.barros7@gmail.com>
2020-02-05 13:26:58 -0500

    This is commit is for 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
-#!/bin/bash
-#include <stdio.h> 
-
-int main ()
+#include <stdio.h>
+int main(void)
 {
-
-    puts ("\"Programming is like building a multilingual puzzle");
-
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
