052e9a3148e044958268976c2b1a453beff19a7e
cbarros7 <cj.barros7@gmail.com>
2020-02-05 15:41:59 -0500

    This commit is for 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,11 @@ float floatType;
 char charType;
 long int longintType;
 long long int long_long_Type;
-printf("Size of a char: %lu byte(s)\n", sizeof(charType));
-printf("Size of an int: %lu byte(s)\n", sizeof(intType));
-printf("Size of a long int: %lu byte(s)\n", sizeof(longintType));
-printf("Size of a a long long int: %lu byte(s)\n", sizeof(long_long_Type));
-printf("Size of a a float: %lu byte(s)\n", sizeof(floatType));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+printf("Size of a a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a a float: %lu byte(s)\n", sizeof(float));
+
 return (0);
 }
