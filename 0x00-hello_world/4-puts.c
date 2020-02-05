3d61104344cc89cab28b720dc480f94d5097dcda
cbarros7 <cj.barros7@gmail.com>
2020-02-05 15:07:29 -0500

    This commit is for 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main - entry point
- * This: print a string
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
