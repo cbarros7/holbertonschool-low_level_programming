17b68c51505d17085784d23c426ec5dd68aebe83
cbarros7 <cj.barros7@gmail.com>
2020-02-05 15:08:16 -0500

    This file is for 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/*
- * This program is for print a string
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
+
 return (0);
 }
