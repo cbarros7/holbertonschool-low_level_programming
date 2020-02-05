07487fcfbfe459700089da5b27e60e14f5b278fa
cbarros7 <cj.barros7@gmail.com>
2020-02-05 15:13:29 -0500

    This file is for 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,6 @@ printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a a float: %lu byte(s)\n", sizeof(float));
+
 return (0);
 }
