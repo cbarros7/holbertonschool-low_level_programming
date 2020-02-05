3143a9af657359b40963e7d758fca3e26a27628b
cbarros7 <cj.barros7@gmail.com>
2020-02-05 12:03:44 -0500

    This commit is for 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h> 
+
+int main ()
+{
+
+    //puts ("\"Programming is like building a multilingual puzzle");
+    printf ("with proper grammar, but the outcome is a piece of art,\n");
+
+    return 0;
+}
