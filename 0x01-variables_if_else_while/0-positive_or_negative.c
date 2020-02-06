8186222b907939746ce988d0a823b07f4b0e1876
cbarros7 <cj.barros7@gmail.com>
2020-02-06 12:29:39 -0500

    This commit is for 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n == 0)
+{
+printf("%i is zero\n", n);
+}
+
+if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+
+else
+printf("%i is positive\n", n);
+
+return (0);
+}
