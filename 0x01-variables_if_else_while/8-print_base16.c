b1adf11ad201818714d7d4766fc7566e1e7f14af
cbarros7 <cj.barros7@gmail.com>
2020-02-06 16:13:28 -0500

    This commit is for 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,17 +9,19 @@ int main(void)
 {
 char base_16; /*Decarling statement*/
 
+/*for base 16 0-9*/
 for (base_16 = 48; base_16 <= 57; base_16++)
 {
-putchar(base_16);
+putchar(base_16); /*print 0-9*/
 }
 
+/*for base 16 a-f*/
 for (base_16 = 97 ; base_16 <= 102; base_16++)
 {
-putchar(base_16);
+putchar(base_16); /*print a-f*/
 }
 
-putchar ('\n');
+putchar ('\n'); /*print new line*/
 
 return (0);
 
