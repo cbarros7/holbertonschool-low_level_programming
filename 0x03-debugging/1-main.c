9e56593d8da1404b1c7db93212f8e23e407ab911
cbarros7 <cj.barros07@gmail.com>
2020-02-11 17:18:02 -0500

    This commit is for 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,8 +14,8 @@ i = 0;
 while (i < 10)
 {
 /**
-* Infinite loop - No increase of variable
-* putchar(i);
+* // Infinite loop - No increase of variable
+* //putchar(i);
 */
 }
 printf("Infinite loop avoided! \\o/\n");
