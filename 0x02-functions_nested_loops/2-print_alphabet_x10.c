4754fe6b2fc42ac598dd4729248eff2d459f1b54
cbarros7 <cj.barros07@gmail.com>
2020-02-10 19:18:32 -0500

    This commit is for 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet_x10(void)
 
 {
 int alphabet; /*Declaring statements*/
@@ -21,7 +21,5 @@ _putchar(alphabet);
 count++;
 _putchar('\n'); /*new line*/
 } /*end while*/
-
-return (0);
 
 }
