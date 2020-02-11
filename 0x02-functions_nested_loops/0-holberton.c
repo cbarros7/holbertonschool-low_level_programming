64e11bbb7f0d9bba57d35b2b9d4ff2f65d7cc8a8
cbarros7 <cj.barros07@gmail.com>
2020-02-11 09:16:47 -0500

    This commit is for holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,9 +9,9 @@ int main(void)
 
 {
 int i; /*Declaring statements*/
-char string [9] = "Holberton"; /*Arrays*/
+char string[9] = "Holberton"; /*Arrays*/
 
-for (i=0 ; i < 9 ; i++) /*Start for*/
+for (i= 0 ; i < 9 ; i++) /*Start for*/
 {
 _putchar(string[i]);/*display word*/
 } /*End for*/
