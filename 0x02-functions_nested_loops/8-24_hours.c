6bcad8eece1ccae2e814ae1c0d507646e8d6c47a
cbarros7 <cj.barros07@gmail.com>
2020-02-11 11:09:42 -0500

    This commit is for 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,32 +9,20 @@ void jack_bauer(void)
 {
 int a;
 int b;
-int c;
-int d;
 
-for (a = 0; a <= 2; a++) /*First for*/
+for (a = 0; a <= 23; a++) /*First for*/
 {
-for (b = 0; b <= 3; b++) /*Second for*/
+for (b = 0; b <= 59; b++) /*Second for*/
 {
-
-for (c = 0; c <= 5; c++) /*Third for*/
-{
-for (d = 0; d <= 9; d++) /*Fourth for*/
-{
-_putchar (a + '0');
-_putchar (b + '0');
+_putchar (a / 10 + '0');
+_putchar (a % 10 + '0');
 _putchar (':');
-_putchar (c + '0');
-_putchar (d + '0');
+_putchar (b / 10 + '0');
+_putchar (b % 10 + '0');
 _putchar ('\n');
 
-} /*End fourth for*/
+}
 
-} /*End third for*/
-
-} /*End second for*/
-
-} /*End first for*/
-
+}
 
 }
