468d3dfa4dc7fe4cf32e0fb51d25f6684e83e81c
cbarros7 <cj.barros07@gmail.com>
2020-02-11 14:30:06 -0500

    This commit is for 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,12 +4,10 @@
  * Return: nothing
  */
 void times_table(void)
-
 {
 int a; /*Declaring statements*/
 int b;
 int c;
-
 for (a = 0; a <= 9; a++)/*First for*/
 {
 for (b = 0; b <= 9; b++) /*Secondo for*/
@@ -17,17 +15,25 @@ for (b = 0; b <= 9; b++) /*Secondo for*/
 c = a * b; /* multiplication a*b*/
 if ((c / 10) == 0) /*If the first digit is 0*/
 {
+if (b == 0)
+{
+_putchar ('0');
+}
+if (b != 0)
+{
 _putchar (' ');
 _putchar ((c % 10) + '0');
-
+}
 if (b < 9) /*don't print the last (;)*/
 {
 _putchar(',');
 _putchar (' ');
 } /*End second IF*/
+
 } /*End the first IF*/
 else
 {
+
 _putchar ((c / 10) + '0');
 _putchar ((c % 10) + '0');
 if (b < 9) /*don't print the last (;)*/
@@ -36,7 +42,6 @@ _putchar(',');
 _putchar (' ');
 }
 } /*End ELSE*/
-
 } /*End second FOR*/
 _putchar ('\n');
 } /*End first FOR*/
