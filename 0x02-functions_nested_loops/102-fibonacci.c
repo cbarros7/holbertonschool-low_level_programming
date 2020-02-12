605a2038fa8f82b71fa8f5bb224f8594b4196770
cbarros7 <cj.barros07@gmail.com>
2020-02-12 00:35:51 -0500

    This commit is for 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+/*Declaring statements*/
+int i;
+int n = 50;
+int i num_1 = 1;
+int num_2 = 2;
+int nextTerm;
+
+for (i = 1; i <= n; ++i) /*Start for*/
+{
+printf("%d, ", num_1);
+nextTerm = num_1 + num_2;
+num_1 = num_2;
+num_2 = nextTerm;
+} /*End for*/
+return 0;
+}
