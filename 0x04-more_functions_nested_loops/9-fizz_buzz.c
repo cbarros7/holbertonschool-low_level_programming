7732802ac0c3108f98175d8ea6f2c6a46f00c35e
cbarros7 <cj.barros07@gmail.com>
2020-02-13 15:58:36 -0500

    This commit is for 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main (void)
+{
+
+	int n = 100; /*Delcaring statements*/
+	int i;
+
+
+	i = 1;
+	while (i <= n) /*Start While*/
+	{
+		if ( i % 3 == 0 ) //Muitple 3*/
+		{
+			printf ("Fizz ");
+		}
+
+		else if ( i % 5 == 0) /*Multple 5*/
+		{
+			printf ("Buzz ");
+		}
+
+		else
+		{
+			printf ("%i ", i); /*Print i*/
+		}
+
+		i++;
+
+	}
+	printf ("\n"); /*New line*/
+
+}
