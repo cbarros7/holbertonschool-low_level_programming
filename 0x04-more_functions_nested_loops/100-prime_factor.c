1b746b83bf88a8851ed6128e942b700a7d67a2fc
cbarros7 <cj.barros07@gmail.com>
2020-02-14 20:50:31 -0500

    This commit is for 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,55 +1,23 @@
 #include <stdio.h>
-#include <math.h>
 
 /**
- * maxPrimeFactors - print the largest prime factor .
+ * main - Entry point.
  *
- *@n: print int
  * Return: Always 0.
  */
 
-/*Declaring function for to find largest prime factor */
-long long maxPrimeFactors(long long n)
+int main(void)
+
 {
-	long long num_max = -1;
+	long n, i; /*Declaring statements*/
 
-	/*Print the number of 2s that divide n */
-	while (n % 2 == 0)
-	{
-		num_max = 2;
-		n >>= 1; /*equivalent to n /= 2 */
-	}
-
-	/*The even numbers and iterate only for odd integers*/
-	for (int i = 3; i <= sqrt(n); i += 2)
+	for (i = 2; i < n; i++) /*Start FOR*/
 	{
 		while (n % i == 0)
-		{
-			num_max = i;
 			n = n / i;
-		}
-	}
+	} /*End For*/
 
-	/*This condition n is a prime number greater than 2 */
-	if (n > 2)
-		num_max = n;
-
-	return (num_max);
-}
-
-/**
- * main - entry point.
- *
- * Return: Always 0.
- */
-
-/*Driver program to test above function */
-int main(void)
-{
-	long long n;
-
-	n = 612852475143;
-	printf("%lld\n", maxPrimeFactors(n));
+	printf("%lu\n", n); /*Print new line*/
 
 	return (0);
 }
