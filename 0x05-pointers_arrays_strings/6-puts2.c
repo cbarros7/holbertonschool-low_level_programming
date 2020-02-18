82e8ef07681e5be0f15a12deb2d58ee419b4a6fb
cbarros7 <cj.barros07@gmail.com>
2020-02-18 12:46:07 -0500

    This commit is for 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,7 @@ void puts2(char *str)
 {
 	int i; /*Declaring variables*/
 
-	for (i = 0; i <= 9; i++) /*Star FOR*/
+	for (i = 0; str[i] != '\0'; i++) /*Star FOR*/
 	{
 		_putchar (str[i]); /*display contents*/
 		i++; /*Incrementing next position*/
