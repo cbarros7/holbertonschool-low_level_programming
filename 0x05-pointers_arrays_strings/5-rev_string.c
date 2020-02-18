8b009524c4ef44dfcc2416ceb6f2b56f0e1f914d
cbarros7 <cj.barros07@gmail.com>
2020-02-18 11:07:56 -0500

    This commit is for 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <string.h>
 /**
  * rev_string - prints a string rev
  *@s: A pointer to an int that will be updated
@@ -12,8 +11,14 @@ void rev_string(char *s)
 	/*Declcaring variables*/
 	char *start_c, *end_c, c;
 	int i, count;
+	int length = 0;
 
-	count = strlen(s); /*Count each character of string*/
+	for(i = 0; s[i]; i++) /*Count character by character*/
+	{
+		length++;
+	} /*End for*/
+
+	count = length; /*Count each character of string*/
 
 	/*Start_c and end_c is equal value of variable s*/
 	start_c = s;
