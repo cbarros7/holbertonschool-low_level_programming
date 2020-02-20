0f031b1bbd4ce286e3b3c3ec85bbc176ff688ce1
cbarros7 <cj.barros07@gmail.com>
2020-02-20 16:47:04 -0500

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 	char *str_two = s2;
 
 	/*WHILE*/
-	while (*str_one != '\0' && !*str_two != '\0' &&  *str_one == *str_two)
+	while (*str_one != '\0' && *str_two != '\0' &&  *str_one == *str_two)
 	{
 		str_one++; /*Add string one and string two*/
 		str_two++;
