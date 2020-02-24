1e69527bd59787b3e3d15d6899a7ce91ec65c2bf
cbarros7 <cj.barros07@gmail.com>
2020-02-24 12:52:21 -0500

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,7 +15,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	/*Delacring FOR*/
 	for (i = 0; i < n; i++)
 	{
-		*(src + i) = *dest; /*add 1 position src*/
+
+		*(dest + i) = *src + i; /*add 1 position dest and src*/
 
 	} /*END FOR*/
 
