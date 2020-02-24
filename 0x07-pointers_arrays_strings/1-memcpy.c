59ad68d2c6debb4b73c5b4ea89964804791f6c18
cbarros7 <cj.barros07@gmail.com>
2020-02-24 13:15:54 -0500

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,10 +15,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	i = 0;
 	while (i < n)  /*Delacring WHILE*/
 	{
-		*(dest + i) = *src + i; /*add 1 position dest and src*/
+		*(dest + i) = *(src + i); /*add 1 position dest and src*/
 		i++;
 
-	} /*END FOR*/
+	} /*END WHILE*/
 
 
 	return (dest);
