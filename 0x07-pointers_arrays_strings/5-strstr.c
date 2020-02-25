2224755f8795ea9d36c2ae0de18b69d5c819bfd1
cbarros7 <cj.barros07@gmail.com>
2020-02-24 20:17:42 -0500

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,13 +9,10 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	char c;
-
-	c = needle[0]; /*first char of needle*/
 
 	while (*haystack != '\0') /*Declaring WHILE*/
 	{
-		if (*haystack == c) /*if s ==  c*/
+		if (*haystack == *needle) /*if s ==  c*/
 		{
 			return (haystack); /*return s*/
 		}
@@ -23,7 +20,7 @@ char *_strstr(char *haystack, char *needle)
 		haystack++;
 	}
 
-	if (*haystack == c)
+	if (*haystack == *needle)
 	{
 		return (haystack);
 
