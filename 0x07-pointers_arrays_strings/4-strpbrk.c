b328aa8a5ef390f94e76124e29ac234cf2f73866
cbarros7 <cj.barros07@gmail.com>
2020-02-24 19:30:09 -0500

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,6 +4,7 @@
  * _strpbrk - matches any character specified
  * @s: This is the C string to be scanned.
  * @accept:character in str1 that matches one of the characters in str2
+ *Return: string s that matches any character specified in accept
  **/
 
 char *_strpbrk(char *s, char *accept)
