b317532d221c2db35b726e0a84a2b893dff6ba39
cbaros7 <cj.baros7@gmail.com>
2020-03-01 23:14:10 -0500

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - funtion
+ *@argc: legenth of argv
+ *@argv: number of argummet
+ *Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	/*Declaring variables*/
+	int position, total, change, aux;
+	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+
+	position = total = change = aux = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	total = atoi(argv[1]); /*Convert str to int*/
+
+	if (total <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	/*Declaring FOR*/
+	for (position = 0; coins[position] != '\0'; position++)
+	{
+		if (total >= coins[position])
+		{
+			aux = (total / coins[position]);
+			change += aux;
+			total -= coins[position] * aux;
+		}
+	}
+	printf("%d\n", change);
+	return (0);
+}
