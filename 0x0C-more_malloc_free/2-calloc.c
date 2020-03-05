a2b71915c79d2d3d7fb7cee9fec7c8e432c071b7
cbarros7 <cj.barros07@gmail.com>
2020-03-05 15:52:43 -0500

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 
-	for (n = 0; n < size; n++)
+	for (n = 0; n < nmemb; n++)
 		*(p + n) = 0;
 
 	return (p);
