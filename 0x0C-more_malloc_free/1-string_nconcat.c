873e2064ad5892a83c30378874c1cfbc9c81bf09
cbarros7 <cj.barros07@gmail.com>
2020-03-05 14:15:37 -0500

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -59,7 +59,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		for (i = 0; *(s1 + i) != '\0'; i++)
 			*(dst + i) = *(s1 + i);
-		for (j = 0; j < n; j++)
+		for (j = 0; j < n && *(s2 + j) != '\0'; j++)
 		{
 			*(dst + i) = *(s2 + j);
 			i++;
