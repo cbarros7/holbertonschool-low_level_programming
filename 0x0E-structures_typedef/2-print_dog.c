4ada430efd180953148d596972b3270bf3622d3f
cbarros7 <cj.barros07@gmail.com>
2020-03-09 18:01:37 -0500

    2--

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,9 +1,6 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "dog.h"
-
-
 
 /**
  * print_dog - function that prints a struct dog
@@ -27,5 +24,6 @@ void print_dog(struct dog *d)
 		printf("Owner: (nil)");
 	else
 		printf("0wner: %s\n", d->owner);
+
 
 }
