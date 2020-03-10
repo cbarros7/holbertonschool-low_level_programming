3e33c1c34cf5d041a9ae042bc7768e5bfc41dc65
cbarros7 <cj.barros07@gmail.com>
2020-03-10 14:25:51 -0500

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,91 +1,86 @@
 #include "dog.h"
 #include <stdlib.h>
-/**
- * _strlen - finds string length in order to malloc properly
- * @str: input string
- * Return: string length unsigned integer
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int i;
 
-	i = 0;
-	while (str[i] != '\0')
+/**
+ * _copy  -   Make a copy
+ * @src:      Data to make copy of
+ * Return:       Pointer copy data other pointer
+ */
+
+char *_copy(char *src)
+{
+	char *ptr;
+	int i, len;
+
+	if (src == NULL)
 	{
-		i++;
+		return (NULL);
 	}
 
-return (i);
+	len = 0;
+	while (src[len] != '\0')
+		len++;
 
+	ptr = malloc(sizeof(char) * (len + 1));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		ptr[i] = src[i];
+	}
+
+	ptr[i] = '\0';
+	return (ptr);
 }
-/**
- * _strcpy - copies string
- * @new_struct: pointer to new string
- * @old_struct: pointer to string to copy
- * Return: void#include "dog.h"
- */
-void _strcpy(char *old_struct, char *new_struct)
-{
-	unsigned int i;
-	unsigned int len_struct;
 
-	len_struct= _strlen(new_struct);
-
-	for (i = 0; i <= len_struct; i++)
-		old_struct[i] = new_struct[i];
-}
 
 /**
- * new_dog - creates new type of struct dog
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
- * Return: pointer to the type of struct dog
+ * new_dog     - Create a new dog variable
+ * @name:        Name of the dog
+ * @age:         Age of the dog
+ * @owner:       Owner of the dog
+ * Return:       Pointer to new dog variable
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int size_name, size_owner;
-
-	size_name = _strlen (name);
-	size_owner = _strlen (owner);
-
 	dog_t *snoopie;
+	char *new_name, *new_owner;
 
 	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
 
-
 	snoopie = malloc(sizeof(dog_t));
+
 	if (snoopie == NULL)
 	{
 		return (NULL);
 	}
-	
-	if (snoopie)
+
+	new_name = _copy(name);
+	if (new_name == NULL)
 	{
-		(*snoopie).name = malloc(sizeof(char) * (size_name + 1));
-		if ((*snoopie).name)
-		{
-			(*snoopie).owner = malloc(sizeof(char) * (size_owner + 1));
-			if ((*snoopie).owner)
-			{
-				/*(*snoopie).name = name;*/
-				_strcpy((*snoopie).name, name);
-				(*snoopie).age = age;
-				_strcpy((*snoopie).owner, owner);
-				/*(*snoopie).owner = owner;*/
-				return (snoopie);
-			}
-			else
-			{
-				free((*snoopie).name);
-				free(snoopie);
-			}
-		}
-		else
-			free(snoopie);
+		free(snoopie);
+		return (NULL);
 	}
-	return (NULL);
+	(*snoopie).name = new_name;
+
+	(*snoopie).age = age;
+
+	new_owner = _copy(owner);
+	if (new_owner == NULL)
+	{
+		free((*snoopie).name);
+		free(snoopie);
+		return (NULL);
+	}
+	(*snoopie).owner = new_owner;
+
+	return (snoopie);
 }
