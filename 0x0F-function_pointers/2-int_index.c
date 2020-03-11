9c53b657f1b28c7467625b3f33d9e464771f39b5
cbarros7 <cj.barros07@gmail.com>
2020-03-11 16:01:02 -0500

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer.
+ *@array: array
+ *@size: size of the array
+ *@cmp:callback function
+ * Return: no element matches -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+
+{
+	int i, j;
+	int count = 0;
+
+	if (array != NULL && cmp != NULL)
+	{
+		if (size <= 0)
+			return (-1);
+
+		i = 0;
+		while (i < size)
+		{
+			count = cmp(array[i]);
+			if (count != 0
+
+				break;
+			i++;
+		}
+
+		return (i);
+
+	}
+
+	return (-i);
+
+}
