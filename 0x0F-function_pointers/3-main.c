4389608657bd48ea33917f7226317c05a4c68cca
cbarros7 <cj.barros07@gmail.com>
2020-03-12 13:45:33 -0500

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,44 +1,41 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+
 #include "3-calc.h"
 
 /**
- * main - Checks for the other codes.
- * @argv: Vector.
- * @argc: Counts the arguments.
- * Return: r.
+ * main - arguments for struct
+ * @argc: numbers arguments
+ * @argv: string arguments
+ * Return: 0.
  */
 
 int main(int argc, char *argv[])
 {
-	/*Delcaring variabls*/
-	int (*oper)(int, int);
-	int convert_1, convert_2, result;
+	int i, j;
+	int (*d)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	oper = get_op_func(argv[2]);
-	if (oper == NULL)
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	convert_1 = atoi(argv[1]);
-	convert_2 = atoi(argv[3]);
+	d = get_op_func(argv[2]);
+		if (d == NULL)
+		{
+			printf("Error\n");
+			exit(99);
+		}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && convert_2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 
-	result = oper(convert_1, convert_2);
-	printf("%d\n", result);
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+
+	printf("%d\n", d(i, j));
+
 	return (0);
 }
