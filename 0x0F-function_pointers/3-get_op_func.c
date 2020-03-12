c2782ed129f6fcaa8a5c60c530412badc1fefcde
cbarros7 <cj.barros07@gmail.com>
2020-03-11 22:23:51 -0500

    3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - Function for  operation.
+ * @s: Operator
+ * Return: Null or position in ops
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+	i++;
+	}
+	return (NULL);
+}
