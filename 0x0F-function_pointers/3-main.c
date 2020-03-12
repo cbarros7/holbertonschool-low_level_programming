4f18348cefe5c729cece235df1b0613a67c32833
cbarros7 <cj.barros07@gmail.com>
2020-03-11 22:23:19 -0500

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+
+int main(int argc, char *argv[])
+{
+	/*Delcaring variabls*/
+	int (*oper)(int, int);
+	int convert_1, convert_2, result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	oper = get_op_func(argv[2]);
+	if (oper == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	convert_1 = atoi(argv[1]);
+	convert_2 = atoi(argv[3]);
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && convert_2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = oper(convert_1, convert_2);
+	printf("%d\n", result);
+	return (0);
+}
