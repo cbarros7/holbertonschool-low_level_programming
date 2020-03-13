dbcac8907a5f326da9d88e5fceb086fa2474df5f
cbarros7 <cj.barros07@gmail.com>
2020-03-13 14:20:26 -0500

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ *@separator: string to be printed between the strings
+ *@n:number of strings passed to the function
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list names;
+	/* initialize valist for num number of arguments */
+	va_start(names, n);
+
+	unsigned int i;
+	const char *array = separator;
+
+	/* access all the arguments assigned to valist */
+	i = 0;
+	while (i < n)
+	{
+		if (separator != NULL)
+		{
+			array = va_arg(names, const char *);
+
+			if (i < n - 1)
+
+				printf("%s, ", array);
+
+			else
+				printf("%s\n", array);
+
+			i++;
+		}
+	}
+
+	/* clean memory reserved for valist */
+	va_end(names);
+
+}
