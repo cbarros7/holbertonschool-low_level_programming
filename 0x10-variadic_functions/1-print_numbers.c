eec9d0e3a4e8307c9ea04793fce29a14aede85ed
cbarros7 <cj.barros07@gmail.com>
2020-03-13 14:20:12 -0500

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers
+ *@separator: string to be printed between numbers
+ *@n:  number of integers passed to the function
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list_num;
+	int result = 0;
+	unsigned int i;
+
+	/* initialize valist for num number of arguments */
+	va_start(list_num, n);
+
+	/* access all the arguments assigned to valist */
+	i = 0;
+	while (i < n)
+	{
+		if (separator != NULL)
+		{
+			result = va_arg(list_num, int);
+
+			if (i < n - 1)
+
+				printf("%i, ", result);
+
+			else
+				printf("%i\n", result);
+
+			i++;
+
+		}
+
+	}
+
+	/* clean memory reserved for valist */
+	va_end(list_num);
+}
