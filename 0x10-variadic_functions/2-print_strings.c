97f200f6ca409143b34fb5661810ffd4dba5f847
cbarros7 <cj.barros07@gmail.com>
2020-03-14 14:19:03 -0500

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,11 +10,11 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list names;
-	/* initialize valist for num number of arguments */
-	va_start(names, n);
-
 	unsigned int i;
 	const char *array = separator;
+
+	/* initialize valist for num number of arguments */
+	va_start(names, n);
 
 	/* access all the arguments assigned to valist */
 	i = 0;
@@ -23,24 +23,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		array = va_arg(names, const char *);
 
 		if (array != NULL)
-
 		{
 			printf("%s", array);
-			if (i < n - 1 && (separator != NULL))
-			printf("%s", separator);
+			if (i < (n - 1) && separator != NULL)
+				printf("%s", separator);
 		}
 
 		else
 		{
 			printf("(nill)");
-			if (i < n - 1 && (separator != NULL))
+			if (i < (n - 1) && separator != NULL)
 				printf("%s", separator);
 		}
-
 		i++;
 	}
 	/* clean memory reserved for valist */
 	va_end(names);
 	printf("\n");
 }
-
