485cd3689f712822a1c3e9265edfa2e1b5adfc00
cbarros7 <cj.barros07@gmail.com>
2020-03-14 19:56:58 -0500

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,4 +8,12 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+
+typedef struct fm
+{
+	char *fm;
+	void (*p)();
+} format_type;
+
+
 #endif
