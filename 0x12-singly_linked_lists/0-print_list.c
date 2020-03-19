3f34d4a85ede507ebc8996f007d458b045041ac6
cbarros7 <cj.barros07@gmail.com>
2020-03-19 17:58:32 -0500

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,12 @@
 
 size_t print_list(const list_t *h)
 {
-	int i = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nill)\n");
+			printf("[0] (nil)\n");
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
