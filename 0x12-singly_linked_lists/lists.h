e1909c00ff38ae24baf30da7373abbc52a329b6f
cbarros7 <cj.barros07@gmail.com>
2020-03-19 13:56:19 -0500

    lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,13 @@
+#ifndef _SINGLY_LINKED_LISTS
+#define _SINGLY_LINKED_LISTS
+
+#include <stdio.h>
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+
+#endif
