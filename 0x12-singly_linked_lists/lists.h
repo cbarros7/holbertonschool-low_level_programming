19dddfdc25606d19b1ba0d93dfc11b9aceb3d57c
cbarros7 <cj.barros07@gmail.com>
2020-03-19 21:28:43 -0500

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -30,5 +30,7 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
+/*Helper functions*/
+char *strdup(const char *s1);
 
 #endif
