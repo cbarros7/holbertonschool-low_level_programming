6f8b8a3869e3962f4533809aad07cd0e4f983ba1
cbarros7 <cj.barros07@gmail.com>
2020-03-19 23:52:43 -0500

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@
 
 void print_fun(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
 
