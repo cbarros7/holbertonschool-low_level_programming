12d625a1afe31da2083d3f757972200862f0d997
cbarros7 <cj.barros07@gmail.com>
2020-03-23 14:56:06 -0500

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,8 +15,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 
@@ -33,5 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 
+/*Helper functions*/
+int _putchar(char c);
 
 #endif
