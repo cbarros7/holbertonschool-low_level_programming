a405795ef4c8c021dd4414f1ef08caffcc050fa3
cbarros7 <cj.barros07@gmail.com>
2020-03-24 07:11:05 -0500

    new fix errors

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (head != NULL)
 	{
 		if (count == index)
-			return(head);
+			return (head);
 
 		head = head->next;
 		count++;
