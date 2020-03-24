ee9e953e1d75287e11a952b898eab740679643e3
cbarros7 <cj.barros07@gmail.com>
2020-03-24 05:36:46 -0500

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -6,7 +6,6 @@
  * @index : index of the node that should be deleted
  * Return: 1 if it succeeded, -1 if it failed
  */
-
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	/*Declaring list and variables*/
