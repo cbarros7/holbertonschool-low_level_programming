be1094d17fd494f917a99646e4405967cf6f2389
cbarros7 <cj.barros07@gmail.com>
2020-03-24 08:06:04 -0500

    Fix errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,12 +36,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (i < (idx - 1))
 	{
 		temp = temp->next;
+		if (temp == NULL && (idx - i) > 0)
+		{
+			return (NULL);
+		}
 		i++;
 	}
 
 	/*Swap*/
-
-
 	new_node->next = temp->next;
 	temp->next = new_node;
 
