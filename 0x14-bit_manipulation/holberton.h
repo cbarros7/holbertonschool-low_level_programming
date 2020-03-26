6e46957425c288cc509d3ad52e3c132dc5d84dd5
cbarros7 <cj.barros07@gmail.com>
2020-03-26 10:39:01 -0500

    Define libraries

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _BIT_MANIPULATION
+#define _BIT_MANIPULATION
+
+#include <stdio.h>
+
+/*Functions*/
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif
