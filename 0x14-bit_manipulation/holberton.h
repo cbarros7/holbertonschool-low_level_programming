6f6dc3d8e1c90d38475d871bf3c9f8f6395cdef0
cbarros7 <cj.barros07@gmail.com>
2020-03-26 16:55:32 -0500

    holberton.h

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,6 +2,10 @@
 #define _BIT_MANIPULATION
 
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
 
 /*Functions*/
 unsigned int binary_to_uint(const char *b);
@@ -10,5 +14,11 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+/*Helper functions*/
+
+int is_numerical(unsigned int n);
+int _atoi(const char *s);
+unsigned int convert_num (const char *b);
 
 #endif
