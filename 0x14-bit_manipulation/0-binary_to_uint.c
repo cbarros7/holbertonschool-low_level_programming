0bc09ed04a450e1248a627c66791b4c8d9b8b158
cbarros7 <cj.barros07@gmail.com>
2020-03-26 17:00:27 -0500

    Fix errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,48 +1,35 @@
 #include "holberton.h"
 
 /**
- * convert_num  - converts string a number
- * @b:  string pointer
- * Return: convertion number
- */
-unsigned int convert_num(const char *b)
-{
-	unsigned int i, len, result = 0;
-
-	len = strlen(b);
-
-	for (i = 0; i < len; i++)
-	{
-		if (b[i] == '0' || b[i] == '1')
-			result = result * 10 + (b[i] - '0');
-
-		else
-			return (0);
-	}
-
-	return (result);
-}
-
-
-/**
  * binary_to_uint  - converts a binary number to an unsigned int
  * @b:  pointing to a string of 0 and 1 chars
  * Return:   converted number or 0 if b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec = 0, i = 0, rem, new, j;
 
-	new = convert_num(b);
+	unsigned int num = 0;
+	int i;
 
-	while (new != 0)
+	/* if string is empty return 0 */
+	if (b == NULL)
+		return (0);
+
+	/* return if the value is different to 0 or 1*/
+	for (i = 0; b[i] != '\0'; i++)
+		if (b[i]  != '0' && b[i] != '1')
+			return (0);
+
+	/* converter array the number binary to  int */
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		rem = new % 10;
-		new /= 10;
-		dec += rem * pow(2, i);
-		++i;
+		num *= 2;
+		if (b[i] == '1')
+			num += 1;
 	}
-	return (dec);
+	return (num);
+
+
 
 }
 
