ecd2492bf0563e03c0b579d2bbf16f887f0bdccb
cbarros7 <cj.barros07@gmail.com>
2020-03-26 22:32:17 -0500

    100-get_endianness.c

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -14,6 +14,7 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 /*Helper functions*/
 void print_num_binary(unsigned long int n);
