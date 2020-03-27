a93e5cf7d2e6d6b0c39f45915bf0107248ef245b
cbarros7 <cj.barros07@gmail.com>
2020-03-26 19:58:05 -0500

    4-clear_bit.c

--- 4-clear_bit.c
+++ 4-clear_bit.c
@@ -6,7 +6,6 @@
  * @index: the index to look for the number
  * Return: 1 if it worked, or -1 if an error occurred
  */
-
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int number = *n;
