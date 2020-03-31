79ffc688a4f48c751af97a099b7d3b141c8b94c5
cbarros7 <cj.barros07@gmail.com>
2020-03-31 12:27:03 -0500

    add function putchar

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -11,7 +11,8 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 
-
+/*Other functions*/
+int _putchar(char c);
 
 
 
