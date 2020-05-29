3da83fb6ce2a76ef20b3d2ad27285972cff8608c
cbarros7 <cj.barros07@gmail.com>
2020-05-29 18:09:09 -0500

    Task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table you want to look into
+ * @key: key string
+ * Return: value associated with the element, or
+ * NULL if key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *ptr;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == 0)
+		return (NULL);
+
+	index = hash_djb2((const unsigned char *) key);
+	index %= ht->size;
+	ptr = ht->array[index];
+
+	while (ptr != NULL)
+	{
+		if (strcmp(ptr->key, key) == 0)
+			break;
+	}
+
+	if (ptr == NULL)
+		return (NULL);
+
+	return (strdup(ptr->value));
+}
