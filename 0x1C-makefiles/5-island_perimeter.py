c606b8c746f901e05823bebbc8f2ef7d4d4d8a3d
cbarros7 <cj.barros07@gmail.com>
2020-07-03 22:50:55 -0500

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,17 +6,16 @@ Island Perimeter
 
 def island_perimeter(grid):
     """Search perimeter"""
-    g = grid
-    result = 0
-    for i in range(len(g)-1):
-        for j in range(1, len(g[0])-1):
-            if (g[i][j-1] == 0 and
-                    g[i][j] == 1 and (g[i][j+1] == 0 or g[i][j+1] == 1)):
-                result += 2
-            if (g[i][j-1] == 0 and
-                    g[i][j] == 1 and g[i][j+1] == 0 and g[i-1][j] == 0):
-                result += 3
-            if (g[i][j-1] == 1 and g[i][j] == 1 and
-                    g[i][j+1] == 0 and g[i-1][j] == 0):
-                result += 3
-    return(result)
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
