c0b1978b3cd878d0cbc0f7ee236328e4c37fa28e
cbarros7 <cj.barros07@gmail.com>
2020-07-03 22:38:18 -0500

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,14 +10,13 @@ def island_perimeter(grid):
     result = 0
     for i in range(len(g)-1):
         for j in range(1, len(g[0])-1):
-            if i != 0:
-                if (g[i][j-1] == 0 and
-                        g[i][j] == 1 and (g[i][j+1] == 0 or g[i][j+1] == 1)):
-                    result += 2
-                if (g[i][j-1] == 0 and
-                        g[i][j] == 1 and g[i][j+1] == 0 and g[i-1][j] == 0):
-                    result += 3
-                if (g[i][j-1] == 1 and g[i][j] == 1 and
-                        g[i][j+1] == 0 and g[i-1][j] == 0):
-                    result += 3
+            if (g[i][j-1] == 0 and
+                    g[i][j] == 1 and (g[i][j+1] == 0 or g[i][j+1] == 1)):
+                result += 2
+            if (g[i][j-1] == 0 and
+                    g[i][j] == 1 and g[i][j+1] == 0 and g[i-1][j] == 0):
+                result += 3
+            if (g[i][j-1] == 1 and g[i][j] == 1 and
+                    g[i][j+1] == 0 and g[i-1][j] == 0):
+                result += 3
     return(result)
